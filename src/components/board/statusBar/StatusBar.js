3bb48346d64293fd7eb77e781035c6a3a47b1dab
Vasyl Popovych <popovich.w.ppc@gmail.com>
2022-10-02 20:41:03 +0300

    ADD functionality for statusBar

--- src/components/board/statusBar/StatusBar.js
+++ src/components/board/statusBar/StatusBar.js
@@ -1,8 +1,31 @@
-import React from "react";
 import styles from "./statusBar.module.scss";
+import { Context } from "../../..";
+import { useContext } from "react";
+import { observer } from "mobx-react-lite";
 
 const StatusBar = () => {
-  return <div className={styles.wrapper}>StatusBar</div>;
+  const { store } = useContext(Context);
+
+  const countOrders = (key) => {
+    return store.ordersData.filter((order) => Object.values(order).includes(key)).length;
+  };
+
+  if (store.ordersData) {
+    return (
+      <div className={styles.wrapper}>
+        <div className={styles.status_bar_item}>Active Tikers: {countOrders(true)} </div>
+        <div className={styles.status_bar_item}>Stocks: {countOrders("stocks")} </div>
+        <div className={styles.status_bar_item}>UNITs: {countOrders("UNIT")}</div>
+      </div>
+    );
+  }
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.status_bar_item}>Active Tikers: </div>
+      <div className={styles.status_bar_item}>Stocks: </div>
+      <div className={styles.status_bar_item}>UNITs: </div>
+    </div>
+  );
 };
 
-export default StatusBar;
+export default observer(StatusBar);
--- src/services/ordersService.js
+++ src/services/ordersService.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const BASE_URL =
-  "https://api.polygon.io/v3/reference/tickers?active=true&sort=ticker&order=asc&limit=100&apiKey=lMuc_oV9L2LNnDjRAvqPSfuOPa3AoQHP";
+const BASE_URL = "https://api.polygon.io/v3/reference/tickers?limit=20&apiKey=lMuc_oV9L2LNnDjRAvqPSfuOPa3AoQHP";
 
 const $api = axios.create({
   baseURL: BASE_URL,
